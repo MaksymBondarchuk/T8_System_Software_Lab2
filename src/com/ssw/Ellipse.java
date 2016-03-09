5eea7a88b2b7e39eec52f50f6bce4898ea4402c5
MaxBondarchuk <bondarchuk.m.y@gmail.com>
2016-03-09 23:46:45 +0200

    With getters, setters, and methods

--- src/com/ssw/Ellipse.java
+++ src/com/ssw/Ellipse.java
@@ -4,9 +4,39 @@ public class Ellipse {
     /**
      * X radius
      */
-    public int a;
+    private int a;
     /**
      * Y radius
      */
-    public int b;
+    private int b;
+
+    public double getXFromY(int y) {
+        return a * Math.sqrt(1 - y * y / b / b);
+    }
+
+    public double getYFromX(int x) {
+        return a * Math.sqrt(1 - x * x / a / a);
+    }
+
+    public void setA(int a) {
+        if (a == 0)
+            throw new ArithmeticException("Ellipse X-radius (A) cannot be 0");
+        else
+            this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setB(int b) {
+        if (b == 0)
+            throw new ArithmeticException("Ellipse Y-radius (B) cannot be 0");
+        else
+            this.b = b;
+    }
+
+    public int getB() {
+        return b;
+    }
 }
--- src/com/ssw/Main.java
+++ src/com/ssw/Main.java
@@ -2,7 +2,13 @@ package com.ssw;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception {
+        Ellipse ellipse = new Ellipse();
+
+        ellipse.setA(7);
+        ellipse.setB(2);
+
+        System.out.println(String.format("A = %d", ellipse.getA()));
+        System.out.println(String.format("B = %d", ellipse.getB()));
     }
 }
